bd2aedb507555186e450ea7e715be018ff79a7a3
cjrace <camrace8@gmail.com>
2020-10-20 21:25:00 +0100

    Fixing a bug where a single fail on the filter group levels test would throw out all filter group pairs and create a list within a list, found other bugs that should all be fixed in the error message of that test now

--- R/mainTests.r
+++ R/mainTests.r
@@ -1306,6 +1306,7 @@ filter_group_match <- function(data, meta) {
 # Checking that filter groups have fewer levels than their filters
 
 filter_group_level <- function(data, meta) {
+
   meta_filters_and_groups <- meta %>%
     filter(col_type == "Filter", !is.na(filter_grouping_column)) %>%
     select(col_name, filter_grouping_column)
@@ -1329,8 +1330,10 @@ filter_group_level <- function(data, meta) {
 
     extended_meta <- suppressWarnings(suppressMessages(meta_filters_and_groups %>% inner_join(filter_levels) %>% inner_join(filter_group_levels) %>% mutate("pre_result" = case_when(filter_levels >= group_levels ~ "PASS", TRUE ~ "FAIL"))))
 
-    number_of_failed_pairs <- extended_meta %>%
-      filter(pre_result == "FAIL") %>%
+    failed_pairs <- extended_meta %>%
+      filter(pre_result == "FAIL")
+    
+    number_of_failed_pairs <- failed_pairs %>%
       nrow()
 
     if (number_of_failed_pairs == 0) {
@@ -1341,12 +1344,12 @@ filter_group_level <- function(data, meta) {
     } else {
       if (number_of_failed_pairs == 1) {
         output <- list(
-          "message" = paste0("The filter group '", paste(extended_meta$filter_grouping_column), "' has more levels (", paste(extended_meta$group_levels), ") than its corresponding filter '", paste(extended_meta$col_name), "' (", paste(extended_meta$filter_levels), "). <br> - This suggests that the hierarchy is the wrong way around in the metadata."),
+          "message" = paste0("The filter group '", paste(failed_pairs$filter_grouping_column), "' has more levels (", paste(failed_pairs$group_levels), ") than its corresponding filter '", paste(failed_pairs$col_name), "' (", paste(failed_pairs$filter_levels), "). <br> - This suggests that the hierarchy is the wrong way around in the metadata."),
           "result" = "FAIL"
         )
       } else {
         output <- list(
-          "message" = paste0("The following filter groups each have more levels than their corresponding filters, check that they are entered the correct way around in the metadata: <br> - '", paste0(extended_meta$filter_grouping_column, collapse = "', '"), "'."),
+          "message" = paste0("The following filter groups each have more levels than their corresponding filters, check that they are entered the correct way around in the metadata: <br> - '", paste0(failed_pairs$filter_grouping_column, collapse = "', '"), "'."),
           "result" = "FAIL"
         )
       }
