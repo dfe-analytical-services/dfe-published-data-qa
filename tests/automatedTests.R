6665cb180b3dc1f921ca9bebf83925bb08a22409
cjrace <camrace8@gmail.com>
2020-10-23 16:12:26 +0100

    shiny test is passing locally now using shiny::runTests()

--- tests/automatedTests.R
+++ tests/automatedTests.R
@@ -1,33 +1,14 @@
 # Script to run both testthat and shinytest tests from command line
 
-# paste0("Starting at: ", getwd())
-# original_dir <- getwd()
-# setwd("c:/R_projects/dfe-published-data-qa") # Need a way to set this as relative to this script somehow?
-# paste0("Moved to: ", getwd())
-# 
-# Sys.unsetenv("http_proxy")
-# Sys.unsetenv("https_proxy")
-# 
-# source("global.R")
-# message("================================================================================")
-# message("== testthat ====================================================================")
-# message("")
-# testthat::test_dir("tests/testthat")
-# message("")
-# message("================================================================================")
-# message("== shinytest ===================================================================")
-# message("")
-# shinytest::testApp(interactive = FALSE)
-# message("")
-# message("================================================================================")
-# 
-# setwd(original_dir)
-# paste0("Ending at: ", getwd())
+library(testthat)
+library(shinytest)
 
+cwd <- getwd()
+setwd("../")
+source("global.R")
+testthat::test_dir("tests/testthat")
+setwd(cwd)
 
-
-## For the shell script
-
-# Rscript -e "originalDir<-getwd();paste0('Starting at: ', getwd());setwd('C:\Agents\RSConnect-Service-01\_work\r99\a\_ees-data-screener-app';paste0('Moved to: '', getwd());source('tests/automatedTests.r);setwd(originalDir);paste0('Ending at: ', getwd())"
-# Rscript -e "originalDir<-getwd();paste0('Starting at: ', getwd());setwd('C:\R_projects\dfe-published-data-qa');paste0('Moved to: '', getwd());source('tests/automatedTests.r);setwd(originalDir);paste0('Ending at: ', getwd())"
-# C:/Agents/RSConnect-Service-01/_work/r99/a/_ees-data-screener-app/tests/automatedTests.r
+test_that("UI tests", {
+  expect_pass(shinytest::testApp("../", compareImages=FALSE, interactive = FALSE))
+})
