ce82cde3a8680e00f0e9161a2c32e93e3f6be4b4
cjrace <camrace8@gmail.com>
2020-11-11 17:24:12 +0000

    finishing off, adding test data for multiple filter groups case

--- R/debugging.R
+++ R/debugging.R
@@ -10,8 +10,8 @@ debugReadFile <- function(file) {
 }
 
 # Put the file paths of the problematic files in here
-datafile <- debugReadFile("C:/R_Projects/dfe-published-data-qa/tests/testthat/mainTests/filter_group_stripped.csv")
-metafile <- debugReadFile("C:/R_Projects/dfe-published-data-qa/tests/testthat/mainTests/filter_group_stripped.meta.csv")
+datafile <- debugReadFile("C:/R_Projects/dfe-published-data-qa/tests/testthat/otherData/multiple_stripped_filter_groups.csv")
+metafile <- debugReadFile("C:/R_Projects/dfe-published-data-qa/tests/testthat/otherData/multiple_stripped_filter_groups.meta.csv")
 
 results <- screenFiles(datafile$filename, metafile$filename, datafile$fileSeparator, metafile$fileSeparator, datafile$fileCharacter, metafile$fileCharacter, datafile$mainFile, metafile$mainFile)
 
