13d682014da54ab354f013101a269f8af979dcd5
cjrace <camrace8@gmail.com>
2021-06-09 21:52:50 +0100

    Adding validation and warning to outlier tab for no indicator selected

--- server.R
+++ server.R
@@ -641,6 +641,10 @@ server <- function(input, output, session) {
 
       # create a list of tables - with one for each indicator summary
       theOutlierList <- eventReactive(input$submit_outlier, {
+        validate(
+          need(input$outlier_indicator_parameter != "", "Please select at least one indicator")
+        )
+        
         return(get_outliers(input$outlier_indicator_parameter, input$threshold_setting, input$ctime_parameter, input$comptime_parameter))
       })
 
