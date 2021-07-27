0f544eae373de3a6e12a92b02c9cd59234357240
lauraselby <laura.selby@hotmail.co.uk>
2021-07-27 14:07:15 +0100

    testing duck

--- global.R
+++ global.R
@@ -256,7 +256,7 @@ getLocalTags <- function() {
       "});"
     )),
     htmltools::tags$div(
-      id="ss-connect-dialog", # style="display: none !important;",
+      id="ss-connect-dialog", style="display: block !important;",
       htmltools::tags$img(id="ss-reload-image"),
       htmltools::tags$a(id="ss-reload-link", href="#", onclick="window.location.reload(true);")
     ),
