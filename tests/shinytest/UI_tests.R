062c8c0fc93eca13a49177e8ef5cfd737896dec8
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2021-07-29 13:25:49 +0100

    merging ui tests into single script

--- tests/shinytest/UI_tests.R
+++ tests/shinytest/UI_tests.R
@@ -1,5 +1,5 @@
 app <- ShinyDriver$new("../../")
-app$snapshotInit("screening_UI", screenshot = FALSE)
+app$snapshotInit("UI_tests", screenshot = FALSE)
 
 # 1. Does it load  -------------------------------------------------------------------------------------------------------------------
 Sys.sleep(1)
@@ -75,3 +75,87 @@ app$snapshot(items = list(
   output = c("advisory_box", "all_tests", "ancillary_box", "failed_box", "passed_box", "data_cols", "data_rows", "data_size", "datafilename", "file_exists", "meta_rows", "meta_cols", "meta_size", "metafilename", "num_advisory_tests", "num_failed_tests", "progress_stage", "showresults", "sum_combined_tests", "sum_failed_tests", "sum_ignored_tests", "sum_passed_tests", "summary_text", "table_advisory_tests", "table_all_tests", "table_failed_tests", "testtime"),
   export = TRUE
 ))
+
+# 11. Do the metadata preview tables populate as expected ------------------
+app$uploadFile(datafile = "test-data/passes_everything.csv")
+app$uploadFile(metafile = "test-data/passes_everything.meta.csv")
+app$setInputs(screenbutton = "click")
+app$setInputs(trendy_tabs = "previewTab")
+app$snapshot(list(output = c("meta_table", "data_preview")))
+
+# 12. Do the file info tables populate as expected -------------------------
+app$setInputs(trendy_tabs = "obUnitTab")
+app$snapshot(list(output = c("geog_time_perms2", "table_1", "indicators", "suppressed_cell_count_table")))
+
+# 13. Explore indicators doesn't break with no arguments -------------------
+app$setInputs(trendy_tabs = "indicatorsTab")
+app$setInputs(submit = "click")
+app$snapshot(list(output = "table_list"))
+
+# 14. Explore indicators doesn't break with just indicators ----------------
+app$setInputs(ind_parameter = "num_schools")
+app$setInputs(submit = "click")
+Sys.sleep(2)
+app$snapshot(list(output = "table_list"))
+
+# 15. Explore indicators doesn't break with just geography -----------------
+app$setInputs(ind_parameter = character(0))
+app$setInputs(geog_parameter = c("National", "Regional", "Local authority", "Local authority district"))
+app$setInputs(submit = "click")
+Sys.sleep(2)
+app$snapshot(list(output = "table_list"))
+
+# 16. Explore indicators works as expected ---------------------------------
+app$setInputs(ind_parameter = c("num_schools", "enrolments"))
+app$setInputs(submit = "click")
+Sys.sleep(2)
+app$snapshot(list(output = c("t_1", "t_2", "t_3", "t_4", "t_5", "t_6", "t_7", "t_8")))
+
+# 17. Outlier check doesn't break with no arguments ------------------------
+app$setInputs(trendy_tabs = "outliersTab")
+app$setInputs(comptime_parameter = "201718")
+app$setInputs(submit_outlier = "click")
+app$snapshot(list(output = "table_outlier_list"))
+
+# 18. Outlier check doesn't break with just indicator ----------------------
+app$setInputs(outlier_indicator_parameter = "num_schools")
+app$setInputs(submit_outlier = "click")
+Sys.sleep(2)
+app$snapshot(list(output = "table_outlier_list"))
+
+# 19. Outlier check doesn't run with same time periods --------------------
+app$setInputs(outlier_indicator_parameter = character(0))
+app$setInputs(comptime_parameter = "201617")
+app$setInputs(submit_outlier = "click")
+Sys.sleep(2)
+app$snapshot(list(output = "table_outlier_list"))
+
+# 20. Outlier check runs as expected --------------------------------------
+app$setInputs(outlier_indicator_parameter = c("num_schools", "enrolments"))
+app$setInputs(threshold_setting = 5)
+app$setInputs(submit_outlier = "click")
+Sys.sleep(2)
+app$snapshot(list(output = c("to_1", "to_2")))
+
+# 21. Geography check runs as expected ------------------------------------
+app$setInputs(trendy_tabs = "geogTab")
+app$setInputs(geog_indicator_parameter = "num_schools")
+app$setInputs(submit_geographies = "click")
+Sys.sleep(2)
+app$snapshot(list(output = "geog_agg2"))
+
+# 22. Hyphen warning appears as expected -----------------------------------
+
+app$setInputs(resetbutton = "click")
+app$uploadFile(datafile = "test-data/hyphen.csv")
+app$uploadFile(metafile = "test-data/hyphen.meta.csv")
+app$setInputs(screenbutton = "click")
+app$setInputs(trendy_tabs = "geogTab")
+app$setInputs(geog_indicator_parameter = "full_time")
+app$setInputs(submit_geographies = "click")
+app$snapshot(list(output = "geog_agg2"))
+
+# 23. Hyphen works for filters on What's in this file ----------------------
+
+app$setInputs(trendy_tabs = "obUnitTab")
+app$snapshot(list(output = c("table_1", "table_2")))
