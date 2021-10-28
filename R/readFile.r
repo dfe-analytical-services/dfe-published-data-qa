639e055af4d317081c2fcfbe50e65a9b9bb29da8
WONG <Sarah.WONG@EDUCATION.GOV.UK>
2021-10-28 16:37:57 +0100

    Allow yyyy-mm-dd files to be uploaded

--- R/readFile.r
+++ R/readFile.r
@@ -1,7 +1,8 @@
 # readFiles --------------------------------------------------
 
 readFile <- function(filePath) {
-  fileSeparator <- capture.output(mainFile <- fread(filePath, encoding = "UTF-8", na.strings = "", verbose = TRUE, strip.white = FALSE)) %>%
+  fileSeparator <- capture.output(mainFile <- fread(filePath, encoding = "UTF-8", na.strings = "", verbose = TRUE, strip.white = FALSE)  %>% 
+                                    mutate_if(is.Date,as.character)) %>%
     .[grepl("sep=.* with", .)] %>%
     sub("with.*$", "", x = .) %>%
     trimws(.) %>%
@@ -11,7 +12,7 @@ readFile <- function(filePath) {
   output <- list(
     "mainFile" = mainFile,
     "fileSeparator" = fileSeparator,
-    "fileCharacter" = fread(filePath, encoding = "UTF-8", colClasses = c("character"), na.strings = NULL)
+    "fileCharacter" = fread(filePath, encoding = "UTF-8", colClasses = c("character"), na.strings = NULL) 
   )
 
   return(output)
