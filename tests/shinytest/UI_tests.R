dae29a0cd6399632746680dc1a22834531512329
BIELBY <Richard.BIELBY@education.gov.uk>
2023-01-04 17:00:32 +0000

    Updated timeouts.

--- tests/shinytest/UI_tests.R
+++ tests/shinytest/UI_tests.R
@@ -9,7 +9,7 @@ app$snapshot()
 app$uploadFile(datafile = "test-data/passes_everything.csv")
 app$uploadFile(metafile = "test-data/passes_everything.meta.csv")
 Sys.sleep(2.8)
-app$setInputs(screenbutton = "click", timeout_ = 8.0e4)
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests")))
 
 app$setInputs(resetbutton = "click")
@@ -18,7 +18,7 @@ app$setInputs(resetbutton = "click")
 app$uploadFile(datafile = "test-data/passes_everything_advisory.csv")
 app$uploadFile(metafile = "test-data/passes_everything_advisory.meta.csv")
 Sys.sleep(2.8)
-app$setInputs(screenbutton = "click", timeout_ = 8.0e4)
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests")))
 
 app$setInputs(resetbutton = "click")
@@ -26,7 +26,7 @@ app$setInputs(resetbutton = "click")
 # 4 - Passes everything ancillary  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/passes_everything_ancillary.csv")
 app$uploadFile(metafile = "test-data/passes_everything_ancillary.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -34,7 +34,7 @@ app$setInputs(resetbutton = "click")
 # 5 - Fail at prescreening stage 2  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/fails_prescreening2.csv")
 app$uploadFile(metafile = "test-data/fails_prescreening2.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -42,7 +42,7 @@ app$setInputs(resetbutton = "click")
 # 6 - Fail at pre-screening stage 1  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/fails_prescreening1.csv")
 app$uploadFile(metafile = "test-data/fails_prescreening1.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -50,7 +50,7 @@ app$setInputs(resetbutton = "click")
 # 7 - Fail at final stage  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/fails_allchecks.csv")
 app$uploadFile(metafile = "test-data/fails_allchecks.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -58,7 +58,7 @@ app$setInputs(resetbutton = "click")
 # 8 - Fail initial file validation  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/fails_file validation.csv")
 app$uploadFile(metafile = "test-data/fails_file validation.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -66,7 +66,7 @@ app$setInputs(resetbutton = "click")
 # 9 - Fail at final stage and ancillary  -------------------------------------------------------------------------------------------------------------------
 app$uploadFile(datafile = "test-data/fails_everything_ancillary.csv")
 app$uploadFile(metafile = "test-data/fails_everything_ancillary.meta.csv")
-app$setInputs(screenbutton = "click")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
 app$snapshot(items = list(output = c("progress_stage", "table_all_tests", "fail_box", "passed_box", "advisory_box", "ancillary_box")))
 
 app$setInputs(resetbutton = "click")
@@ -81,68 +81,68 @@ app$snapshot(items = list(
 # 11. Do the metadata preview tables populate as expected ------------------
 app$uploadFile(datafile = "test-data/passes_everything.csv")
 app$uploadFile(metafile = "test-data/passes_everything.meta.csv")
-app$setInputs(screenbutton = "click", timeout_ = 8.0e4)
-app$setInputs(trendy_tabs = "previewTab")
+app$setInputs(screenbutton = "click", timeout_ = 1.6e4)
+app$setInputs(trendy_tabs = "previewTab", timeout_ = 1.6e4)
 app$snapshot(list(output = c("meta_table", "data_preview")))
 
 # 12. Do the file info tables populate as expected -------------------------
-app$setInputs(trendy_tabs = "obUnitTab")
+app$setInputs(trendy_tabs = "obUnitTab", timeout_ = 1.6e4)
 app$snapshot(list(output = c("geog_time_perms2", "table_1", "indicators", "suppressed_cell_count_table")))
 
 # 13. Explore indicators doesn't break with no arguments -------------------
 app$setInputs(trendy_tabs = "indicatorsTab")
-app$setInputs(submit = "click")
+app$setInputs(submit = "click", timeout_ = 1.6e4)
 app$snapshot(list(output = "table_list"))
 
 # 14. Explore indicators doesn't break with just indicators ----------------
 app$setInputs(ind_parameter = "num_schools")
-app$setInputs(submit = "click")
+app$setInputs(submit = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = "table_list"))
 
 # 15. Explore indicators doesn't break with just geography -----------------
 app$setInputs(ind_parameter = character(0))
 app$setInputs(geog_parameter = c("National", "Regional", "Local authority", "Local authority district"))
-app$setInputs(submit = "click")
+app$setInputs(submit = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = "table_list"))
 
 # 16. Explore indicators works as expected ---------------------------------
 app$setInputs(ind_parameter = c("num_schools", "enrolments"))
-app$setInputs(submit = "click")
+app$setInputs(submit = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = c("t_1", "t_2", "t_3", "t_4", "t_5", "t_6", "t_7", "t_8")))
 
 # 17. Outlier check doesn't break with no arguments ------------------------
 app$setInputs(trendy_tabs = "outliersTab")
 app$setInputs(comptime_parameter = "201718")
-app$setInputs(submit_outlier = "click")
+app$setInputs(submit_outlier = "click", timeout_ = 1.6e4)
 app$snapshot(list(output = "table_outlier_list"))
 
 # 18. Outlier check doesn't break with just indicator ----------------------
 app$setInputs(outlier_indicator_parameter = "num_schools")
-app$setInputs(submit_outlier = "click")
+app$setInputs(submit_outlier = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = "table_outlier_list"))
 
 # 19. Outlier check doesn't run with same time periods --------------------
 app$setInputs(outlier_indicator_parameter = character(0))
 app$setInputs(comptime_parameter = "201617")
-app$setInputs(submit_outlier = "click")
+app$setInputs(submit_outlier = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = "table_outlier_list"))
 
 # 20. Outlier check runs as expected --------------------------------------
 app$setInputs(outlier_indicator_parameter = c("num_schools", "enrolments"))
 app$setInputs(threshold_setting = 5)
-app$setInputs(submit_outlier = "click")
+app$setInputs(submit_outlier = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = c("to_1", "to_2")))
 
 # 21. Geography check runs as expected ------------------------------------
 app$setInputs(trendy_tabs = "geogTab")
 app$setInputs(geog_indicator_parameter = "num_schools")
-app$setInputs(submit_geographies = "click")
+app$setInputs(submit_geographies = "click", timeout_ = 1.6e4)
 Sys.sleep(2)
 app$snapshot(list(output = "geog_agg2"))
 
