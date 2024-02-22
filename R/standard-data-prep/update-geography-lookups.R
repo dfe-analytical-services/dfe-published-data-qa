4504dddfae8a3bfff05f8bffe7747f38b975cb5b
Rich Bielby <richard.bielby@education.gov.uk>
2024-02-22 17:02:06 +0000

    tidy code

--- R/standard-data-prep/update-geography-lookups.R
+++ R/standard-data-prep/update-geography-lookups.R
@@ -3,7 +3,7 @@ library(readr)
 #       https://geoportal.statistics.gov.uk/search?q=LUP_WPC&sort=Date%20Created%7Ccreated%7Cdesc
 #       e.g. data/downloaded_source_data/Ward_to_Westminster_Parliamentary_Constituency_to_Local_Authority_District_to_Upper_Tier_L.csv
 update_pcon_la_lookup <- function(open_geography_file, existing_data_file = "data/la_pcon_hierarchy.csv") {
-  new_data <- read_csv(open_geography_file) 
+  new_data <- read_csv(open_geography_file)
   new_year <- names(new_data) %>%
     as.data.frame() %>%
     filter(grepl("PCON", .), grepl("CD", .)) %>%
@@ -21,12 +21,12 @@ update_pcon_la_lookup <- function(open_geography_file, existing_data_file = "dat
     ) %>%
     mutate(
       la_name = if_else(
-        grepl("E11",new_la_code) | grepl("E12",new_la_code)| grepl("E13",new_la_code),
+        grepl("E11", new_la_code) | grepl("E12", new_la_code) | grepl("E13", new_la_code),
         lad_name,
         la_name
       ),
       new_la_code = if_else(
-        grepl("E11",new_la_code) | grepl("E12",new_la_code)| grepl("E13",new_la_code),
+        grepl("E11", new_la_code) | grepl("E12", new_la_code) | grepl("E13", new_la_code),
         lad_code,
         new_la_code
       )
@@ -60,8 +60,7 @@ update_pcon_la_lookup <- function(open_geography_file, existing_data_file = "dat
 }
 
 # @param open_geography_file Lookup file sourced from
-#       https://geoportal.statistics.gov.uk/search?q=LUP_WPC&sort=Date%20Created%7Ccreated%7Cdesc
-#       e.g. data/downloaded_source_data/Ward_to_Westminster_Parliamentary_Constituency_to_Local_Authority_District_to_Upper_Tier_L.csv
+#       https://geoportal.statistics.gov.uk/search?q=lsip&sort=Title%7Ctitle%7Casc
 update_lad_lsip_lookup <- function(open_geography_file, existing_data_file = "data/lsip_lad_hierarchy.csv") {
   new_data <- read_csv(open_geography_file)
   new_year <- names(new_data) %>%
