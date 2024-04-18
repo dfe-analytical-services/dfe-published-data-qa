4352b4649ea4fe491d356a85d15702872c6125a2
cjrace <camrace8@gmail.com>
2024-04-18 08:14:29 +0100

    Update comments around the LA / LAD special case handling

--- R/standard-data-prep/utils.R
+++ R/standard-data-prep/utils.R
@@ -90,17 +90,26 @@ tidy_downloaded_lookup <- function(
   }
 
   # Extra tidy up for separating LAs / LADs
-  # Not 100% on the logic but it seems to be filtering / sifting between LAs and LADs - is that right Rich?
+  # This bit's to deal with LAs and UTLAs not being quite the same thing. 
+  # The parent UTLA for LADs in metropolitan counties (E11, e.g. Manchester),
+  # and Inner or Outer London (E13) are the Metropolitan counties 
+  # and Inner / Outer London. 
+  # But in these cases, the LA for our purposes is the LAD itself.
+  
+  # So this is using the LAD as it's own parent LA if it's in a metropolitan
+  # county or in London and taking the UTLA otherwise.
+  
+  So all this logic is doing is using the LAD as it's own parent LA if it's in a metropolitan county or in London and taking the UTLA otherwise.
   if ("new_la_code" %in% names(new_lookup)) {
     new_lookup1 <- new_lookup %>%
       mutate(
         la_name = if_else(
-          grepl("E11", new_la_code) | grepl("E12", new_la_code) | grepl("E13", new_la_code),
+          grepl("E11", new_la_code) | grepl("E13", new_la_code),
           lad_name,
           la_name
         ),
         new_la_code = if_else(
-          grepl("E11", new_la_code) | grepl("E12", new_la_code) | grepl("E13", new_la_code),
+          grepl("E11", new_la_code) | grepl("E13", new_la_code),
           lad_code,
           new_la_code
         )
