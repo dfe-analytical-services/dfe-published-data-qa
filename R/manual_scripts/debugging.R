d7fd794529e658af442ee712436f00e2d29ecf30
cjrace <camrace8@gmail.com>
2024-05-03 17:03:14 +0100

    update debugging script to account for shiny now autoloading

--- R/manual_scripts/debugging.R
+++ R/manual_scripts/debugging.R
@@ -1,7 +1,18 @@
 # Debugging script
 # Use this script to manually read in and screen files for those occasions when the app breaks without a useful error
+# Helpful for running code in console
+# Uncomment when you need to use this, and always make sure to comment it again before committing
 
+# # Dependencies ==============================================================
 # source("global.R")
+# source("R/readFile.R")
+# source("R/screenFiles.R")
+# source("R/knownVariables.R")
+# source("R/fileValidation.R")
+# source("R/preCheck1.R")
+# source("R/preCheck2.R")
+# source("R/mainTests.R")
+#
 #
 # debugReadFile <- function(file) {
 #   output <- readFile(file)
@@ -9,14 +20,16 @@
 #   return(output)
 # }
 #
-# # Put the file paths of the files in here
+# # Choose files ==============================================================
 # datafile <- debugReadFile(file.choose())
 # metafile <- debugReadFile(file.choose())
 #
+# # Screen files ==============================================================
 # results <- screenFiles(datafile$filename, metafile$filename, datafile$fileSeparator, metafile$fileSeparator, datafile$fileCharacter, metafile$fileCharacter, datafile$mainFile, metafile$mainFile)
 #
 # data.table(results$results) %>% View()
 #
+# # Extra helpers =============================================================
 # data <- datafile$mainFile
 # meta <- metafile$mainFile
 #
