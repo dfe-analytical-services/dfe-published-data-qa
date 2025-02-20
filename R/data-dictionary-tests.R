e1d1850d47300b5ccefa77f774af0a251fbbde96
Rich Bielby <richbielby@e.email>
2025-02-20 15:02:59 +0000

    Updated data dictionary col_name check

--- R/data-dictionary-tests.R
+++ R/data-dictionary-tests.R
@@ -1,3 +1,9 @@
+# Data dictionary
+data_dictionary <- function() {
+  readr::read_csv("data/data-dictionary.csv")
+}
+
+
 # Check filter col_name entries against the data dictionary col_name listing
 #
 # @description All filter col_names should be correspond to entries in the data
@@ -5,24 +11,51 @@
 # entries against the data dictionary.
 data_dictionary_filter_col_name_check <- function(meta) {
   # Collapse search terms for bad column names into regex term
-  non_standard_col_names <- meta %>%
-    select(col_name, filter_grouping_column) %>%
-    pivot_longer(
+  data_dictionary <- data_dictionary()
+  dd_col_names <- data_dictionary |>
+    dplyr::select(col_type, col_name, col_parent_name) |>
+    tidyr::pivot_longer(
+      c(col_name, col_parent_name),
+      values_to = "col_name"
+    ) |>
+    dplyr::select(-name) |>
+    dplyr::distinct() |>
+    dplyr::arrange(col_type, col_name) |>
+    dplyr::mutate(standard_col = TRUE)
+
+  non_standard_col_names <- meta |>
+    dplyr::select(col_type, col_name, filter_grouping_column) |>
+    tidyr::pivot_longer(
       c(col_name, filter_grouping_column),
       values_to = "col_name"
-    ) %>%
-    filter(!(col_name %in% standard_col_names)) %>%
-    pull(col_name)
-  if (length(non_standard_col_names) == 0) {
+    ) |>
+    dplyr::left_join(dd_col_names, by = dplyr::join_by(col_type, col_name)) |>
+    dplyr::filter(!is.na(col_name), is.na(standard_col)) |>
+    dplyr::distinct()
+
+  if (nrow(non_standard_col_names) == 0) {
     output <- list(
       "message" = "All col_names are consistent with the data dictionary.",
       "result" = "PASS"
     )
   } else {
+    non_standard_indicators <- non_standard_col_names |>
+      dplyr::filter(col_type == "Indicator") |>
+      dplyr::pull("col_name") |>
+      paste(collapse = ", ")
+    non_standard_filters <- non_standard_col_names |>
+      dplyr::filter(col_type == "Filter") |>
+      dplyr::pull("col_name") |>
+      paste(collapse = ", ")
     output <- list(
-      "message" = paste0(
-        "The column(s) '",
-        paste(non_standard_col_names, collapse = "', '"), "' are not present in the data dictionary and should not be used as part of an API data set until resolved."
+      "message" = paste(
+        "The folling column(s) are not present in the data dictionary",
+        "and should not be used as part of an API data set until resolved:\n",
+        "Indicators:",
+        ifelse(non_standard_indicators != "", non_standard_indicators, "PASSED"),
+        "\n",
+        "Filters:",
+        ifelse(non_standard_filters != "", non_standard_filters, "PASSED")
       ),
       "result" = "WARNING"
     )
