6454ea2daff3d69fee77fa2055e1c518bbf74996
Rich Bielby <richard.bielby@education.gov.uk>
2025-10-10 17:14:15 +0100

    Data dictionary/key stage 2 (#163)
    * Added API data checker function
    
    * Added KS2 filters to data dictionary
    
    * Adding indicators for KS2 to data dictionary
    
    * Test updates
    
    * Updating first language entry in data dictionary
    
    * Update R/api-data-checker.r
    
    Co-authored-by: Cam Race <52536248+cjrace@users.noreply.github.com>
    
    * Update R/api-data-checker.r
    
    Co-authored-by: Cam Race <52536248+cjrace@users.noreply.github.com>
    
    * Update R/api-data-checker.r
    
    Co-authored-by: Cam Race <52536248+cjrace@users.noreply.github.com>
    
    * Completed context column in data dictionary
    
    * Moved api checker example to top of script
    
    ---------
    
    Co-authored-by: Cam Race <52536248+cjrace@users.noreply.github.com>

---
+++ R/api-data-checker.r
@@ -0,0 +1,97 @@
+# This is a standalone function not run by the screener itself.
+# I'm using it to quickly create a list of column names and filter items from
+# a set of data files and sort them into a data-dictionary structure with
+# flags as to whether they're currently in the data dictionary or not.
+
+checker_example_run <- function() {
+  dir <- "tests/testthat/test-data/"
+  files <- list.files(dir)
+  files <- paste0(dir, files[!grepl("meta", files)])
+  listing <- api_data_checker(files)
+  return(non_dd_rows(listing))
+}
+
+api_data_checker <- function(files) {
+  entries <- data.frame(
+    col_name = NA,
+    col_type = NA,
+    filter_item = NA,
+    col_name_parent = NA,
+    filter_item_parent = NA
+  )
+  for (file in files) {
+    data <- vroom::vroom(file, show_col_types = FALSE)
+    meta <- vroom::vroom(gsub(".csv", ".meta.csv", file), show_col_types = FALSE)
+    filters <- meta |>
+      dplyr::filter(col_type == "Filter") |>
+      dplyr::select("col_name", "filter_grouping_column")
+    for (i in 1:nrow(filters)) {
+      columns <- c(
+        filters$col_name[i],
+        filters$filter_grouping_column[i]
+      )
+      filter_info <- data |>
+        dplyr::select(all_of(columns[!is.na(columns)])) |>
+        dplyr::distinct() |>
+        dplyr::mutate(col_name = paste0(filters$col_name[i]))
+      if (!is.na(filters$filter_grouping_column[i])) {
+        filter_info <- filter_info |>
+          dplyr::mutate(
+            col_name_parent = paste0(filters$filter_grouping_column[i])
+          ) |>
+          dplyr::rename(filter_item_parent = filters$filter_grouping_column[i])
+      } else {
+        filter_info <- filter_info |>
+          dplyr::mutate(filter_item_parent = "", col_name_parent = "")
+      }
+      entries <- entries |>
+        dplyr::bind_rows(
+          filter_info |>
+            dplyr::select(
+              col_name,
+              filter_item = filters$col_name[i],
+              col_name_parent,
+              filter_item_parent
+            ) |>
+            dplyr::mutate(col_type = "Filter")
+        ) |>
+        dplyr::bind_rows(
+          meta |> dplyr::filter(col_type == "Indicator") |> dplyr::select(col_name, col_type)
+        ) |>
+        dplyr::mutate(across(everything(), ~ dplyr::if_else(is.na(.x), "", .x)))
+    }
+  }
+  return(
+    entries |>
+      dplyr::filter(!is.na(col_name)) |>
+      dplyr::select(
+        col_name,
+        col_type,
+        filter_item,
+        col_name_parent,
+        filter_item_parent,
+      ) |>
+      dplyr::arrange(
+        col_type,
+        col_name,
+        col_name_parent,
+        filter_item_parent,
+        filter_item
+      ) |>
+      dplyr::distinct()
+  )
+}
+
+non_dd_rows <- function(listing) {
+  dd <- vroom::vroom("data/data-dictionary.csv", show_col_types = FALSE) |>
+    dplyr::select(
+      col_name,
+      col_type,
+      filter_item,
+      col_name_parent,
+      filter_item_parent
+    ) |>
+    dplyr::mutate(across(everything(), ~ dplyr::if_else(is.na(.x), "", .x)))
+  listing |>
+    dplyr::anti_join(dd)
+}
