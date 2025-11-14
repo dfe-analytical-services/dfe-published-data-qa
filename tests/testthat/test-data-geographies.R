e4ca28ca18058d38ff60d98b4aa0237009189160
Rich Bielby <richbielby@e.email>
2025-11-14 13:11:44 +0000

    Ordered LAs file and added tests to make sure all E06 and E08 areas are consistently present across the LA and LAD data sets.

---
+++ tests/testthat/test-data-geographies.R
@@ -0,0 +1,33 @@
+las <- read.csv("../../data/las.csv") |>
+  dplyr::select(new_la_code, la_name) |>
+  dplyr::filter(grepl("E06", new_la_code))
+lads <- read.csv("../../data/lads.csv") |>
+  dplyr::select(lad_code, lad_name) |>
+  dplyr::filter(grepl("E06", lad_code))
+
+
+test_that("Unitary authorities (E06) match between LA and LAD look-ups", {
+  expect_equal(
+    las |>
+      dplyr::select(new_la_code, la_name) |>
+      dplyr::filter(grepl("E06", new_la_code)) |>
+      nrow(),
+    lads |>
+      dplyr::select(lad_code, lad_name) |>
+      dplyr::filter(grepl("E06", lad_code)) |>
+      nrow()
+  )
+})
+
+test_that("Metropolitan boroughs (E08) match between LA and LAD look-ups", {
+  expect_equal(
+    las |>
+      dplyr::select(new_la_code, la_name) |>
+      dplyr::filter(grepl("E08", new_la_code)) |>
+      nrow(),
+    lads |>
+      dplyr::select(lad_code, lad_name) |>
+      dplyr::filter(grepl("E08", lad_code)) |>
+      nrow()
+  )
+})
